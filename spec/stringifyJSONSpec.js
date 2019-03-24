f413bfcaf1054fedb206b64ced8307ae14403df8
dustintktran <dustintktran@gmail.com>
2019-03-24 13:39:54 -0600

    Update. All complete but stringifyJSON unstringifyable doesnt work

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -16,3 +16,44 @@ describe('stringifyJSON', function() {
 
   });
 });
+ // var arrayBegin = true;
+var stringifyJSON = function(input){
+  var array = [];
+  if(typeof input == "string"){
+    var string = '"' + input + '"';
+    return string;
+  } else if(input == null){
+    return "null";
+  } else if(typeof input == "number"){
+    var string = "" + input;
+    return string;
+  } else if(typeof input == "boolean"){
+    var string = "" + input;
+    return string;
+  } else if(typeof input == "object"){
+    if(Array.isArray(input)){
+      if(input.length == 0){
+        return "[]";
+      } else {
+        input.forEach(function(value) {
+          array.push(stringifyJSON(value));
+        });
+        return "[" + array + "]";
+      }
+    } else {
+      var keys = Object.keys(input);
+      var values = Object.values(input);
+      for(var i = 0; i < keys.length; i++){
+        if((values[i]) === null || keys[i] === null);
+        return "{}";
+      }
+      if(keys.length==0){
+        return "{}";
+      }
+      for(var i = 0; i < keys.length; i++){
+      array.push(stringifyJSON(keys[i]) + ":" + stringifyJSON(values[i]));
+      } 
+      return "{" + array + "}";
+    }
+  }
+}
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,51 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
+// var getElementsByClassName = function (className) 
 //   return document.getElementsByClassName(className);
 // };
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+	var getClasses = function(node){
+		var classes = node.classList;
+		return classes;
+	}
+	var doTheThing = function(child){
+		for(var i = 0; i < child.length; i++){
+		var classes = getClasses(child[i]);
+		if(classes.contains(className) && !child[i].hasChildNodes()){
+			output.push(child[i])
+		} else if(!classes.contains(className) && child[i].hasChildNodes()){
+			doTheThing(child[i].children);
+		} else if(classes.contains(className) && child[i].hasChildNodes()){
+			output.push(child[i]);
+			doTheThing(child[i].children);
+		}
+	}
+	return output;
+	}
+	var output = [];
+	var main = document.body;
+	var child = main.children;
+	doTheThing(child);
+	if(output.length !== 0){
+		output.unshift(main);
+	}
+	return output;
+
 };
+//classList = DOMTokenList ["targetClassName", value: "targetClassName"]
+/*  var body = document.body;
+  var nodes = body.children;
+  var classList = nodes.classList;
+ // _forEach(nodes, function(node){
+
+ // })
+  for(var i = 0; i < nodes.length; i++){
+  //	var classes = $('nodes[i]').attr('className').split(' ');
+  	if(nodes[i].className == className){
+  		output.push(body);
+  		output.push(nodes[i]);
+  	}
+  }
+  return output;*/
